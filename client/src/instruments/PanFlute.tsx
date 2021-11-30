7783af57405dae838e76126647418593dde98f4c
andyouy <aouyang@mail.sfsu.edu>
2021-11-30 13:05:12 -0800

    panflute update

--- client/src/instruments/PanFlute.tsx
+++ client/src/instruments/PanFlute.tsx
@@ -3,9 +3,10 @@ import * as Tone from 'tone';
 import classNames from 'classnames';
 import { List, Range } from 'immutable';
 import React from 'react';
+import fluteKey from '../img/bambooimage.jpg'
 
 // project imports
-import { Instrument, InstrumentProps } from '../Instruments';
+import { Instrument } from '../Instruments';
 
 /** ------------------------------------------------------------------------ **
  * Contains implementation of components for Piano.
@@ -35,10 +36,10 @@ export function PanFluteKey({
     // 1. The JSX refers to the HTML-looking syntax within TypeScript.
     // 2. The JSX will be **transpiled** into the corresponding `React.createElement` library call.
     // 3. The curly braces `{` and `}` should remind you of string interpolation.
-    <div
+    <img
       onMouseDown={() => synth?.triggerAttack(`${note}`)} // Question: what is `onMouseDown`?
       onMouseUp={() => synth?.triggerRelease('+0.25')} // Question: what is `onMouseUp`?
-      className={classNames('ba pointer absolute dim', {
+      className={classNames('pointer absolute dim', {
         'black bg-white': !minor, // major keys are white
       })}
       style={{
@@ -50,7 +51,9 @@ export function PanFluteKey({
         marginLeft: minor ? '0.25rem' : 0,
         height: `${index + 2}rem`,
       }}
-    ></div>
+      src={fluteKey}
+      alt=""
+    ></img>
   );
 }
 
@@ -66,14 +69,12 @@ function PianoKeyWithoutJSX({
    * See `PianoKey` for the React component with JSX (JavaScript XML).
    */
   return React.createElement(
-    'div',
+    'img',
     {
       onMouseDown: () => synth?.triggerAttack(`${note}`),
       onMouseUp: () => synth?.triggerRelease('+0.25'),
-      className: classNames('ba pointer absolute dim', {
-        'bg-black black': minor,
-        'black bg-white': !minor,
-      }),
+      className: classNames(' pointer absolute dim',),
+      src: "./img/jGlzr.png",
       style: {
         top: 0,
         left: `${index * 2}rem`,
@@ -87,21 +88,10 @@ function PianoKeyWithoutJSX({
   );
 }
 
-function PanFluteType({ title, onClick, active }: any): JSX.Element {
-  return (
-    <div
-      onClick={onClick}
-      className={classNames('dim pointer ph2 pv1 ba mr2 br1 fw7 bw1', {
-        'b--black black': active,
-        'gray b--light-gray': !active,
-      })}
-    >
-      {title}
-    </div>
-  );
-}
 
-function PanFlute({ synth, setSynth }: InstrumentProps): JSX.Element {
+function PanFlute(): JSX.Element {
+
+  
   const keys = List([
     { note: 'C', idx: 0 },
     //{ note: 'Db', idx: 1 },
@@ -117,33 +107,37 @@ function PanFlute({ synth, setSynth }: InstrumentProps): JSX.Element {
     { note: 'B', idx: 6 },
   ]);
 
-  const setOscillator = (newType: Tone.ToneOscillatorType) => {
-    setSynth(oldSynth => {
-      oldSynth.disconnect();
-
-      return new Tone.Synth({
-        oscillator: { type: newType } as Tone.OmniOscillatorOptions,
+  const synth = new Tone.Synth({
+    "volume": 1,
+    "detune": 0,
+    "portamento": 0.09,
+    "envelope": {
+    "attack": 5,
+    "attackCurve": "sine",
+    "decay": 0.3,
+    "decayCurve": "exponential",
+    "release": 1,
+    "releaseCurve": "exponential",
+    "sustain": 0.2
+    },
+    "oscillator": {
+    "partialCount": 4,
+    "partials": [
+      1,
+      0.586181640625,
+      0.019775390625,
+      0.000244140625
+    ],
+    "phase": 0
+    },
       }).toDestination();
-    });
-  };
 
-  const oscillators: List<OscillatorType> = List([
-    'sine',
-    'sawtooth',
-    'square',
-    'triangle',
-    'fmsine',
-    'fmsawtooth',
-    'fmtriangle',
-    'amsine',
-    'amsawtooth',
-    'amtriangle',
-  ]) as List<OscillatorType>;
+
 
   return (
-    <div className="pv4">
-      <div className="relative dib h4 w-100 ml4">
-        {Range(2, 5).map(octave =>
+    <div className="pv3">
+      <div className="relative dib h4 w-75" style={{right: "200px"}}>
+        {Range(3, 5).map(octave =>
           keys.map(key => {
             //const isMinor = key.note.indexOf('b') !== -1;
             const note = `${key.note}${octave}`;
@@ -159,16 +153,6 @@ function PanFlute({ synth, setSynth }: InstrumentProps): JSX.Element {
             );
           }),
         )}
-      </div>
-      <div className={'pl4 pt7 flex'}>
-        {oscillators.map(o => (
-          <PanFluteType
-            key={o}
-            title={o}
-            onClick={() => setOscillator(o)}
-            active={synth?.oscillator.type === o}
-          />
-        ))}
       </div>
     </div>
   );
--- client/src/visualizers/TwoDWaveform.tsx
+++ client/src/visualizers/TwoDWaveform.tsx
@@ -5,9 +5,13 @@ import * as Tone from 'tone';
 // project imports
 import { Visualizer } from '../Visualizers';
 
-
+let hasDrawn = 0;
+let xDirection = 0;
+let xValue = 5;
+let yDirection = 0;
+let yValue = 5;
 export const TwoDWaveformVisualizer = new Visualizer(
-  'Waveform',
+  'TwoDWaveform',
   (p5: P5, analyzer: Tone.Analyser) => {
     const width = window.innerWidth;
     const height = window.innerHeight / 2;
@@ -15,9 +19,31 @@ export const TwoDWaveformVisualizer = new Visualizer(
 
     p5.background(0, 0, 0, 255);
 
-    p5.strokeWeight(dim * 0.01);
+    p5.noStroke();
+    p5.strokeWeight(dim*0.01);
     p5.stroke(150, 70, 120, 150);
     p5.noFill();
+  
+
+    function getRandom(value: number){
+      return Math.floor(Math.random() * value);
+    }
+
+    function directionChange(){
+      if(xDirection <= -800){
+        xValue = 5;
+      }else if(xDirection >= 700){
+        xValue = -5;
+      }
+    }
+
+    function yChange(){
+      if(yDirection >= 200){
+        yValue = -5;
+      } else if(yDirection <= -200){
+        yValue = 5;
+      }
+    }
 
     const values = analyzer.getValue();
     p5.beginShape();
@@ -25,9 +51,56 @@ export const TwoDWaveformVisualizer = new Visualizer(
       const amplitude = values[i] as number;
       const x = p5.map(i, 0, values.length - 1, 0, width);
       const y = height / 2 + amplitude * height;
+
+      if(hasDrawn === 0){
+        p5.translate(800, height/2)
+        p5.push()
+        p5.rect(50 + xDirection , 50 + yDirection, 50 + x * amplitude * getRandom(10), 50 + y * amplitude * getRandom(10));
+        p5.pop()
+
+        p5.push()
+        p5.rect(0 + xDirection , 100 + yDirection, 50 + x * amplitude * getRandom(10), 50 + y * amplitude * getRandom(10));
+        p5.pop()
+
+        p5.push()
+        p5.rect(100 + xDirection , 100 + yDirection, 50 + x * amplitude * getRandom(10), 50 + y * amplitude * getRandom(10));
+        p5.pop()
+
+        p5.push()
+        p5.rect(100 + xDirection ,0 + yDirection, 50 + x * amplitude * getRandom(10), 50 + y * amplitude * getRandom(10));
+        p5.pop()
+
+        p5.push()
+        p5.rect(0 + xDirection , 0 + yDirection, 50 + x * amplitude * getRandom(10), 50 + y * amplitude * getRandom(10));
+        p5.pop()
+        
+        p5.push()
+        p5.circle(75 + xDirection ,75 + yDirection,50 + y * amplitude * getRandom(10))
+        p5.pop();
+
+        hasDrawn = 1;
+        directionChange();
+        yChange();
+      }
+    
+      
       // Place vertex
-      p5.vertex(x, y);
+      // if(i%5 === 0){
+      //   p5.rect(x, y, 20, 100 * getRandom(amplitude))
+      // }
+
+        //p5.point(x * getRandom(x), y * getRandom(y))
+
+        //p5.triangle(width/2 - 300 + amplitude * getRandom(width), 375 + amplitude * getRandom(height), width/2 + amplitude * getRandom(width), 220 + amplitude * getRandom(height), width/2 + 300 +amplitude * getRandom(width), 375 + amplitude * getRandom(height));
+
     }
+    // const amplitude = values[10] as number;
+
+    xDirection += xValue;
+    yDirection += yValue;
     p5.endShape();
+    hasDrawn = 0;
+
+    
   },
 );
