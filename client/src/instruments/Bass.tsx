82955f533f5fa540bca92bebe01e9ee09cb0ac2e
JeffreyFriedrich <jefriedrich4@gmail.com>
2021-12-03 11:18:30 -0800

    implemented bass instrument

--- client/src/instruments/Bass.tsx
+++ client/src/instruments/Bass.tsx
@@ -6,29 +6,30 @@ import React from 'react';
 
 // project imports
 import { Instrument, InstrumentProps } from '../Instruments';
+import backgroundImage from '../img/wood-background3.png';
+import pick from '../img/pick.png';
 
 /** ------------------------------------------------------------------------ **
- * Contains implementation of components for Piano.
+ * Contains implementation of components for Bass.
  ** ------------------------------------------------------------------------ */
 
-interface PianoKeyProps {
+interface BassKeyProps {
   note: string; // C, Db, D, Eb, E, F, Gb, G, Ab, A, Bb, B
   duration?: string;
   synth?: Tone.Synth; // Contains library code for making sound
-  minor?: boolean; // True if minor key, false if major key
-  octave: number;
-  index: number; // octave + index together give a location for the piano key
+  string: number; // affects verticality and starting octave/key
+  index: number; // octave + index together give a location for the bass key
 }
 
-export function PianoKey({
+export function BassKey({
   note,
   synth,
-  minor,
+  string,
   index,
-}: PianoKeyProps): JSX.Element {
+}: BassKeyProps): JSX.Element {
   /**
-   * This React component corresponds to either a major or minor key in the piano.
-   * See `PianoKeyWithoutJSX` for the React component without JSX.
+   * This React component corresponds to either a major or minor key in the bass.
+   * See `BassKeyWithoutJSX` for the React component without JSX.
    */
   return (
     // Observations:
@@ -39,54 +40,23 @@ export function PianoKey({
       onMouseDown={() => synth?.triggerAttack(`${note}`)} // Question: what is `onMouseDown`?
       onMouseUp={() => synth?.triggerRelease('+0.25')} // Question: what is `onMouseUp`?
       className={classNames('ba pointer absolute dim', {
-        'bg-black black h3': minor, // minor keys are black
-        'black bg-white h4': !minor, // major keys are white
+        'black bg-white': 1, // major keys are white
       })}
       style={{
         // CSS
-        top: 0,
-        left: `${index * 2}rem`,
-        zIndex: minor ? 1 : 0,
-        width: minor ? '1.5rem' : '2rem',
-        marginLeft: minor ? '0.25rem' : 0,
+        top: `${string * 2}rem`,
+        height: '2rem',
+        left: `${index * 4}rem`,
+        zIndex: string, // z-index determines whats on top; should be by string
+        width: '4rem',
+        background: `url(${backgroundImage})`,
+        cursor: `url(${pick}), auto`
       }}
     ></div>
   );
 }
 
-// eslint-disable-next-line
-function PianoKeyWithoutJSX({
-  note,
-  synth,
-  minor,
-  index,
-}: PianoKeyProps): JSX.Element {
-  /**
-   * This React component for pedagogical purposes.
-   * See `PianoKey` for the React component with JSX (JavaScript XML).
-   */
-  return React.createElement(
-    'div',
-    {
-      onMouseDown: () => synth?.triggerAttack(`${note}`),
-      onMouseUp: () => synth?.triggerRelease('+0.25'),
-      className: classNames('ba pointer absolute dim', {
-        'bg-black black h3': minor,
-        'black bg-white h4': !minor,
-      }),
-      style: {
-        top: 0,
-        left: `${index * 2}rem`,
-        zIndex: minor ? 1 : 0,
-        width: minor ? '1.5rem' : '2rem',
-        marginLeft: minor ? '0.25rem' : 0,
-      },
-    },
-    [],
-  );
-}
-
-function PianoType({ title, onClick, active }: any): JSX.Element {
+function BassType({ title, onClick, active }: any): JSX.Element {
   return (
     <div
       onClick={onClick}
@@ -100,20 +70,31 @@ function PianoType({ title, onClick, active }: any): JSX.Element {
   );
 }
 
-function Piano({ synth, setSynth }: InstrumentProps): JSX.Element {
-  const keys = List([
-    { note: 'C', idx: 0 },
-    { note: 'Db', idx: 0.5 },
-    { note: 'D', idx: 1 },
-    { note: 'Eb', idx: 1.5 },
-    { note: 'E', idx: 2 },
-    { note: 'F', idx: 3 },
-    { note: 'Gb', idx: 3.5 },
-    { note: 'G', idx: 4 },
-    { note: 'Ab', idx: 4.5 },
-    { note: 'A', idx: 5 },
-    { note: 'Bb', idx: 5.5 },
-    { note: 'B', idx: 6 },
+function Bass({ synth, setSynth }: InstrumentProps): JSX.Element {
+  interface bassKey {
+    note: string,
+  }
+
+  const keys: bassKey[] = [
+    { note: 'C'},
+    { note: 'Db'},
+    { note: 'D'},
+    { note: 'Eb'},
+    { note: 'E'},
+    { note: 'F'},
+    { note: 'Gb'},
+    { note: 'G'},
+    { note: 'Ab'},
+    { note: 'A'},
+    { note: 'Bb'},
+    { note: 'B'},
+  ];
+
+  const strings = List([
+    {position: 0, firstOctave: 3, keyOffset: 7}, // firstNote: 'G'
+    {position: 1, firstOctave: 2, keyOffset: 2}, //firstNote: 'D'
+    {position: 2, firstOctave: 2, keyOffset: 9}, //firstNote: 'A',
+    {position: 3, firstOctave: 2, keyOffset: 4}, //firstNote: 'E',
   ]);
 
   const setOscillator = (newType: Tone.ToneOscillatorType) => {
@@ -142,26 +123,31 @@ function Piano({ synth, setSynth }: InstrumentProps): JSX.Element {
   return (
     <div className="pv4">
       <div className="relative dib h4 w-100 ml4">
-        {Range(2, 7).map(octave =>
-          keys.map(key => {
-            const isMinor = key.note.indexOf('b') !== -1;
-            const note = `${key.note}${octave}`;
-            return (
-              <PianoKey
-                key={note} //react key
-                note={note}
-                synth={synth}
-                minor={isMinor}
-                octave={octave}
-                index={(octave - 2) * 7 + key.idx}
-              />
-            );
-          }),
-        )}
+          {strings.map(
+            string =>
+              Range(0, 22).map(
+                fret =>
+                  {
+                    const localKey = keys[(string.keyOffset + fret) % 12];
+                    const octave = string.firstOctave + Math.floor((string.keyOffset + fret) / 12);
+                    const note = `${localKey.note}${octave}`;
+                    return (
+                      <BassKey
+                        key={note} //react key
+                        note={note}
+                        synth={synth}
+                        string={string.position}
+                        index={fret}
+                      />
+                    );
+                  },
+                
+              )
+          )}
       </div>
       <div className={'pl4 pt4 flex'}>
         {oscillators.map(o => (
-          <PianoType
+          <BassType
             key={o}
             title={o}
             onClick={() => setOscillator(o)}
@@ -173,4 +159,4 @@ function Piano({ synth, setSynth }: InstrumentProps): JSX.Element {
   );
 }
 
-export const BassInstrument = new Instrument('Bass', Piano);
+export const BassInstrument = new Instrument('Bass', Bass);
