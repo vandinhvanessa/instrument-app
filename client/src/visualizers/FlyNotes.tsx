d3d5b9e83bce8b9f04328abe2b1caf808a055078
JeffreyFriedrich <jefriedrich4@gmail.com>
2021-12-05 22:13:22 -0800

    simple visualizer implemented

--- client/src/visualizers/FlyNotes.tsx
+++ client/src/visualizers/FlyNotes.tsx
@@ -5,7 +5,10 @@ import * as Tone from 'tone';
 // project imports
 import { Visualizer } from '../Visualizers';
 
-
+let xDirection = 0;
+let xValue = 10;
+let yDirection = 0;
+let yValue = 0;
 export const FlyNotesVisualizer = new Visualizer(
   'FlyNotes',
   (p5: P5, analyzer: Tone.Analyser) => {
@@ -13,21 +16,52 @@ export const FlyNotesVisualizer = new Visualizer(
     const height = window.innerHeight / 2;
     const dim = Math.min(width, height);
 
-    p5.background(0, 0, 0, 255);
+    function getRandom(value: number){
+      return Math.floor(Math.random() * value);
+    }
 
-    p5.strokeWeight(dim * 0.01);
-    p5.stroke(255, 255, 255, 255);
-    p5.noFill();
+    function directionChange(){
+      // if(xDirection <= -width/2){
+      if(xDirection <= -width/2 && yValue == 0){
+        yValue = 10;
+        xValue = 0;
+      // } else if(yDirection >= height) {
+      } else if(yDirection >= height/2 && xValue == 0) {
+        yValue = 0;
+        xValue = 10;
+      // } else if(xDirection >= width/3){
+      } else if(xDirection >= width/2 - 250 && yValue == 0){
+        yValue = -10;
+        xValue = 0;
+      // } else if(yDirection <= height/2) {
+      } else if(yDirection <= -height/2 && xValue == 0) {
+        yValue = 0;
+        xValue = -10;
+      }
+    }
+
+    // p5.background(0, 0, 0, 255);
+
+    p5.strokeWeight(dim * .01);
+
+    p5.fill(getRandom(255), getRandom(255), getRandom(255));
 
     const values = analyzer.getValue();
     p5.beginShape();
     for (let i = 0; i < values.length; i++) {
+      let angle = p5.map(i, 0, values.length, 0, 360)
       const amplitude = values[i] as number;
-      const x = p5.map(i, 0, values.length - 1, 0, width);
-      const y = height / 2 + amplitude * height;
-      // Place vertex
-      p5.vertex(x, y);
+      let radius = p5.map(amplitude * height, 0, 256, 50, 300)
+      const x = radius * p5.cos(angle) + width/2;
+      const y = radius * p5.sin(angle) + height/2;
+
+      p5.stroke(getRandom(255 * amplitude), getRandom(255 * amplitude), getRandom(255 * amplitude));
+      p5.curveVertex(x + xDirection, y + yDirection);
     }
+    directionChange();
+    xDirection += xValue;
+    yDirection += yValue;
+
     p5.endShape();
   },
 );
