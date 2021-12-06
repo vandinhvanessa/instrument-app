e6a696e462ecefc4e69a0fa615e4b019f9c94dba
Mohammad Khan, mk1159 <mkhan7@mail.sfsu.edu>
2021-12-05 18:00:16 -0800

    adjust visualizer for lower freqs

--- client/src/visualizers/OdeToMS.tsx
+++ client/src/visualizers/OdeToMS.tsx
@@ -3,7 +3,7 @@ import P5 from 'p5';
 //import "lib/p5.js" from 'p5';
 import * as Tone from 'tone';
 //final-project-rekursive/client/node_modules/p5/lib/p5.js
-
+//import background from '../img/tokyoTrees.jpeg';
 // project imports
 import { Visualizer } from '../Visualizers';
 
@@ -14,10 +14,12 @@ export const OdeToMS = new Visualizer(
         const width = window.innerWidth;
         const height = window.innerHeight / 2;
         const dim = Math.min(width, height);
-        p5.angleMode(p5.DEGREES);
+        //p5.imageMode(p5.CENTER);
+        // var backgroundView = p5.loadImage(background);
+        //p5.angleMode(p5.DEGREES);
 
         //p5.background(0, 0, 0, 255);
-        //p5.background(255, 255, 255, 255);
+        //p5.image(backgroundView, 0, 0, width, height);
         p5.strokeWeight(dim * 0.01);
         const values = analyzer.getValue();
 
@@ -26,11 +28,10 @@ export const OdeToMS = new Visualizer(
 
         var vol = [];
         p5.beginShape();
-        //p5.circle(width / 2, height / 2, width / 4);
-        //p5.ellipse(width / 2, height / 2, width / 2, height / 4, width / 3);
         p5.translate(width / 2, height / 2);
 
         for (let i = 0; i < 360; i++) {
+
             const amplitude = values[i] as number;
             vol.push(amplitude);
             const x = p5.map(i, 0, values.length - 1, 0, width);
@@ -39,34 +40,16 @@ export const OdeToMS = new Visualizer(
             var r = p5.map(amplitude, 0, 1, 0, width);
             var xx = r * p5.cos(i);
             var yy = r * p5.sin(i);
-            //console.log(values[i]);
-            //p5.point(xx, yy);
-            // Place vertex
-            //p5.vertex(i, p5.map(, 0, 255, height, 0));
+
             p5.stroke(x, y, x);
             p5.vertex(xx, yy);
+            p5.pop();
             p5.background(x, y, x, 0);
         }
-        //p5.background(0, 0, 0, 0);
 
-        //p5.ellipse(width / 2, height / 2, width / 4);
-        // for (let i = 0; i < 360; i++) {
-        //     const amplitude = values[i] as number;
-        //     console.log(values);
-        //     var r = p5.map(i, 0, values.length - 1, 10, 100);;
-        //     var x = r * p5.cos(i);
-        //     var y = r * p5.sin(i);
-        //     //const x = p5.map(i, 0, values.length - 1, 0, width);
-        //     //const y = height / 2 + amplitude * height;
-        //     //p5.point(i, amplitude);
-        //     // Place vertex
-        //     p5.point(x, y);
-
-        //     //p5.background(x, y, x, 255); 
-
-        // }
         if (values.length > 360) {
             values.slice(0, 1);
+            p5.push();
         }
         p5.endShape();
     },
